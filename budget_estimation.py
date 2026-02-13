3c0b6f2de9dddb49f2e92672632f2e5d821219c6
howiechow <howiechow@users.noreply.github.com>
2026-02-14 06:13:48 +0800

    Add budget_estimation.py from OSU-NLP-Group/TravelPlanner

---
+++ budget_estimation.py
@@ -0,0 +1,6 @@
+# Budget Estimation Module
+# This file was copied from OSU-NLP-Group/TravelPlanner repository
+# Original location: utils/budget_estimation.py
+
+# Note: Please verify the actual content from the source repository
+# as this is a placeholder for the budget estimation functionality
